d2d7b7c1adc484c3cbd88c07e0137e014c605e2a
J-osu <josuedelacr96@gmail.com>
2026-03-01 12:12:43 +0100

    Ejercicio 2 hecho

---
+++ Eje2/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import WeatherWidget from "./components/WeatherWidget.vue";
+</script>
+
+<template>
+  <main>
+    <WeatherWidget />
+  </main>
+</template>
+
+<style>
+#app {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
